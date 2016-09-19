476bbaece2072975b9af81235b319cb193ca6ad8
zgy03916618421 <576428698@qq.com>
2016-09-19 18:26:54 +0800

    format

--- service/voiceservice.js
+++ service/voiceservice.js
@@ -5,6 +5,8 @@ var qiniu = require('node-qiniu');
 var request = require('request');
 exports.voiceHandle = function *(postBody,token) {
     var mediaid = postBody.xml.MediaId[0];
+    var format = postBody.xml.Format[0];
+    console.log(format)
     console.log(mediaid);
     mongodb.collection('message').insertOne(postBody);
     qiniu.config({
@@ -12,7 +14,7 @@ exports.voiceHandle = function *(postBody,token) {
         secret_key : 'JRqORkckw7iGjVedNJNSyO5ND-88EmWObL-gOYiU'
     })
     var Bucket = qiniu.bucket('zhougy');
-    var puttingStream = Bucket.createPutStream(mediaid+'.mp4');
+    var puttingStream = Bucket.createPutStream(mediaid+'.'+format);
     request.get('https://api.weixin.qq.com/cgi-bin/media/get?access_token='+token+'&media_id='+mediaid).pipe(puttingStream)
         .on('error', function(err) {
             console.error(err);
