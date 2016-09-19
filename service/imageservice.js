cf3c1fc7a6e7098ef0943f908b3160a7c7b28af6
zgy03916618421 <576428698@qq.com>
2016-09-19 16:38:56 +0800

    weiqu qiu quan

--- service/imageservice.js
+++ service/imageservice.js
@@ -2,7 +2,8 @@
  * Created by Administrator on 2016/9/18.
  */
 var wxAPI = require('./wxapiservice');
-var qiniu = require('node-qiniu')
+var qiniu = require('node-qiniu');
+var request = require('request');
 exports.imageHandle = function *(postBody,token) {
     var openid = postBody.xml.FromUserName[0];
     var url = postBody.xml.PicUrl[0];
@@ -10,14 +11,13 @@ exports.imageHandle = function *(postBody,token) {
     var mediaid = postBody.xml.MediaId[0];
     console.log(mediaid);
     mongodb.collection('message').insertOne({'openid':openid,'msgtype':'image','picurl':url});
-    var img = yield wxAPI.getMdia(token,mediaid);
     qiniu.config({
         access_key : 'h8OIBtpUPHoMUU03hz63fU8NfHgQ4brAtno9kbjN',
         secret_key : 'JRqORkckw7iGjVedNJNSyO5ND-88EmWObL-gOYiU'
     })
     var Bucket = qiniu.bucket('zhougy');
     var puttingStream = Bucket.createPutStream('wechat_test');
-    img.pipe(puttingStream)
+    request.get(url).pipe(puttingStream)
         .on('error', function(err) {
             console.error(err);
         })
